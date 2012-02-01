d39d75302e0e2eab30a20873b0f1f720f84fac35
Malcolm Locke <malc@wholemeal.co.nz>
2012-02-01 22:12:24 +1300

    Reload apache on virtualhost change

--- modules/apache/manifests/virtualhost.pp
+++ modules/apache/manifests/virtualhost.pp
@@ -2,8 +2,10 @@ define apache::virtualhost($documentroot,
                            $bindto = '*:80',
                            $servername = $name,
                            $serveraliases = false) {
+
   file {"/etc/apache2/sites-enabled/$name":
-    #content => "<VirtualHost *:80>\nServerName $name\nDocumentRoot $documentroot\n</VirtualHost>\n",
     content => template('apache/virtualhost.erb'),
+    notify  => Service["apache2"],
   }
+
 }
