708b2fb7739072a0e1ff70a0d3395cef56c2e3b9
Malcolm Locke <malc@wholemeal.co.nz>
2012-02-08 23:47:14 +1300

    GHOSTCHIPS!

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -2,4 +2,10 @@ class apache {
   package {"apache2": }
 
   service {"apache2": }
+
+  file {"/etc/apache/sites-available/default":
+    source  => "puppet:///modules/apache/default",
+    require => Package["apache2"],
+    notify  => Service["apache2"],
+  }
 }
